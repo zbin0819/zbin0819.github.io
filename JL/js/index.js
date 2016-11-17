f924b468ff4597b352861fab4b323ed2e4eb4548
zb <714591092@qq.com>
2016-11-17 22:00:40 +0800

    简历即将完成

--- JL/js/index.js
+++ JL/js/index.js
@@ -6,17 +6,18 @@ $(function(){
 		audioBtn = $('#audio'),
 		switchPage = $('.switch-page'),
 		activeLine = $('.bottom-text span'),
+		skillActiveImg = $('.skill-img img'),
 		lineActive = $('.personal-info .line');
-	// console.log(activeLine);
+		
 	setTimeout(function(){				//设置头像延时旋转
 		imgRotate.addClass('img-rotate');
 		activeLine.addClass('line-active');
 	},2000);
-	var mySwiper = new Swiper ('.swiper-container', {
+	var mySwiper = new Swiper ('.swiper-container-wrap', {
 	    direction: 'vertical',
 	    // loop: true,
 	    // 如果需要分页器
-	    pagination: '.swiper-pagination',
+	    pagination: '.swiper-pagination-right',
 	    paginationType : 'progress',
 	    // 如果需要滚动条
 	    // scrollbar: '.swiper-scrollbar',
@@ -31,6 +32,8 @@ $(function(){
 	    	activeLine.removeClass('line-active');
 	    	activeLine.css({display:'none'});
 	    	lineActive.removeClass('line-active');
+	    	skillActiveImg.removeClass('active-img');
+	    	skillActiveImg.css({display:'none'});
 	    	if(swiper.activeIndex == 0){
 	    		setTimeout(function(){	//页面切换后设置头像延时旋转
 	    			imgRotate.addClass('img-rotate');
@@ -48,9 +51,31 @@ $(function(){
 	    	if(swiper.activeIndex == 1){
 	    		lineActive.addClass('line-active');
 	    	}
+	    	if(swiper.activeIndex == 2){
+	    		setTimeout(function(){
+		    		skillActiveImg.css({display:'block'});
+		    		skillActiveImg.addClass('active-img');
+	    		},500)
+	    	}
+	    	if(swiper.activeIndex == 3){
+	    		
+	    	}
 	    	swiperAnimate(swiper); 		//每个slide切换结束时也运行当前slide动画
 	    },
   	});
+  	var swiperH = new Swiper('.swiper-container-h', {  
+  		loop:true,
+  		pagination: '.swiper-pagination-h',
+	    paginationType : 'bullets',
+	    effect : 'cube',
+//	    autoplay:4000,
+		cube: {
+			slideShadows: true,
+			shadow: true,
+			shadowOffset: 100,
+			shadowScale: 0.6
+		}
+    }); 
   	//音乐按钮
   	musicBtn.click(function(){
   		if($(this).data('btn') != true){
